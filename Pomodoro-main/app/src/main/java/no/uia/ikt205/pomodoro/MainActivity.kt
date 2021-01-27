7e255be408ada2502b1badb4abae24cf74a9e9e8
Othniel Timothy De Mesa Luzon <otluzon@uia.no>
2021-01-27 10:50:57 +0100

    added new features to app

--- Pomodoro-main/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ Pomodoro-main/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -13,7 +13,14 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var timer:CountDownTimer
     lateinit var startButton:Button
-    lateinit var coutdownDisplay:TextView
+    lateinit var add30:Button
+    lateinit var add60:Button
+    lateinit var add90:Button
+    lateinit var add120:Button
+    lateinit var countdownDisplay:TextView
+
+    var realTime: Long = 0L
+    var flag = false
 
     val timeToCountDownInMs = 5000L
     val timeTicks = 1000L
@@ -22,18 +29,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       startButton = findViewById<Button>(R.id.startCountdownButton)
-       startButton.setOnClickListener(){
-           startCountDown(it)
-       }
-       coutdownDisplay = findViewById<TextView>(R.id.countDownView)
+        startButton = findViewById<Button>(R.id.startCountdownButton)
+        startButton.setOnClickListener(){
+            startCountDown(it)
+        }
+        add30 = findViewById<Button>(R.id.add30Button)
+        add30.setOnClickListener(){
+            changeCountDown(30)
+        }
+
+        add60 = findViewById<Button>(R.id.add60Button)
+        add60.setOnClickListener(){
+            changeCountDown(60)
+        }
+        add90 = findViewById<Button>(R.id.add90Button)
+        add90.setOnClickListener(){
+            changeCountDown(90)
+        }
+        add120 = findViewById<Button>(R.id.add120Button)
+        add120.setOnClickListener(){
+            changeCountDown(120)
+        }
+
+        countdownDisplay = findViewById<TextView>(R.id.countDownView)
 
     }
 
     fun startCountDown(v: View){
 
-        timer = object : CountDownTimer(timeToCountDownInMs,timeTicks) {
+        if(flag){
+            timer.cancel()
+        }
+
+        flag = true
+
+        timer = object : CountDownTimer(realTime,timeTicks) {
             override fun onFinish() {
+                flag = false
                 Toast.makeText(this@MainActivity,"Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
             }
 
@@ -46,7 +78,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun updateCountDownDisplay(timeInMs:Long){
-        coutdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
+        countdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
+    }
+
+    fun changeCountDown(Multiplier:Long){
+        realTime = timeToCountDownInMs/5 * 60 * Multiplier
     }
 
 }

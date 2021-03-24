11e40514cdb62aad84681867f920f6732b3643ce
Othniel Timothy De Mesa Luzon <otluzon@uia.no>
2021-03-24 15:01:17 +0100

    added project to repository

---
+++ MyList/app/src/main/java/ikt205/project/mylist/MainActivity.kt
@@ -0,0 +1,11 @@
+package ikt205.project.mylist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

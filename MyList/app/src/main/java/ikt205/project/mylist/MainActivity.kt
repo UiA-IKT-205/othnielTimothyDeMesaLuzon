399912ce7029a8673b9f66865abbc7435b7b71ff
Othniel Timothy De Mesa Luzon <otluzon@uia.no>
2021-03-30 16:11:23 +0200

    added binding to MainActivity and listEntryListing

--- MyList/app/src/main/java/ikt205/project/mylist/MainActivity.kt
+++ MyList/app/src/main/java/ikt205/project/mylist/MainActivity.kt
@@ -2,10 +2,18 @@ package ikt205.project.mylist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ikt205.project.mylist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding.listEntryListing
+
+
     }
 }

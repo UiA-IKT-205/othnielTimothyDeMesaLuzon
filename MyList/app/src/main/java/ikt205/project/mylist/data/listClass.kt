f559f7617a3d543074a24e90104610bdad64777e
timothyluzon <70034267+timothyluzon@users.noreply.github.com>
2021-04-14 17:40:25 +0200

    Revert "Edit activity"

--- MyList/app/src/main/java/ikt205/project/mylist/data/listClass.kt
+++
@@ -1,7 +0,0 @@
-package ikt205.project.mylist.data
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-class TodoList(val title: String, val entries: MutableList<String>): Parcelable
